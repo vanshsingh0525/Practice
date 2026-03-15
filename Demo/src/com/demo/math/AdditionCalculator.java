1f6ea6890148c5ee8244ae420ef20c33b7070a42
vanshu0525 <vanshslathia03@gmail.com>
2026-03-15 13:29:20 +0530

    [Vansh] : added program for adding two numbers

--- Demo/src/com/demo/math/AdditionCalculator.java
+++ Demo/src/com/demo/math/AdditionCalculator.java
@@ -2,15 +2,12 @@ package com.demo.math;
 
 public class AdditionCalculator {
 
-    public static int add(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
-    }
-
-    public static void main(String[] args) {
-        int firstNumber = 10;
-        int secondNumber = 20;
-        int sum = add(firstNumber, secondNumber);
-
-        System.out.println("Sum: " + sum);
-    }
+   public static void main(String[] args) {
+	
+	   int a = 5;
+	   int b = 10;
+	   int result = a+b;
+	   System.out.println(result);
+}
+ 
 }
